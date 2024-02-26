dbbc7ed8880438da6cb1debdccb9f8e6efdcd417
Ogaraga <raymond.ogaraga@gmail.com>
2024-02-26 21:54:34 +0100

    location tracker

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,36 +1,38 @@
 import styles from "./Login.module.css";
 import mage from "../assets/arrow.png";
 import { Link, useNavigate } from "react-router-dom";
-import { useContext} from "react";
+import { useContext } from "react";
 import axios from "axios";
 import UserContextApi from "../context/userContext";
 
 const Login = () => {
-  
-    const {user, setUser} = useContext(UserContextApi);
-    
+  const { user, setUser } = useContext(UserContextApi);
+
   const handleChange = (ev) => {
-     setUser({ ...user, [ev.target.name]: ev.target.value });
+    setUser({ ...user, [ev.target.name]: ev.target.value });
   };
   const navigate = useNavigate();
   const handleSubmit = async (ev) => {
     ev.preventDefault();
     const fetchData = async () => {
       await axios
-        .post("http://localhost:5000/login",user)
-        .then(res => setUser(res.data)).catch(err=>console.log(err));
+        .post("http://localhost:5000/login", user)
+        .then((res) => setUser(res.data))
+        .catch((err) => console.log(err));
     };
-    
-    
+
     // let myData = [];
     // myData.push(Users);
     // myData.map((item, idx) => {<span key={idx}>
     //   <p>{item.email} </p>
     //   <p>{item.password} </p>
     // </span>});
-    
-    if (fetchData() && user.password !== user.confirmPassword ||
-    user.password.length < 3) {
+
+    if (
+      fetchData() &&
+      user.password !== user.confirmPassword &&
+      user.password.length < 3
+    ) {
       document.getElementById("lab").innerHTML = "Invalid user";
       document.getElementById("lab").style.color = "red";
       navigate("/login");
@@ -42,7 +44,7 @@ const Login = () => {
       }, 5000);
     }
   };
-  
+
   return (
     <>
       <nav>
@@ -82,7 +84,7 @@ const Login = () => {
           <label id="lab"></label>
           <div className={styles.btn}>
             <p>Don`t have account</p>
-            <img src={mage} alt="Arrow" className ={styles.img} />
+            <img src={mage} alt="Arrow" className={styles.img} />
             <Link to="/">
               <button type="button" className={styles.uniq}>
                 sign up
